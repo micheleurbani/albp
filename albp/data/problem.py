d1eacca46cc55875ed98b34ad482d6b46c92cb5c
Michele Urbani <michele@didatec.com>
2024-04-10 11:37:56 +0200

    Added order strength to read ALB.

--- albp/data/problem.py
+++ albp/data/problem.py
@@ -67,7 +67,7 @@ class Problem:
                 elif "<number of stations>" in row:
                     raise NotImplementedError
                 elif "<order strength>" in row:
-                    self.OS = float(f.readline().strip())
+                    self.OS = float(f.readline().strip().replace(",", "."))
                 elif "<task times>" in row:
                     self.t = np.array([int(f.readline().strip().split(" ")[-1])
                                        for _ in range(self.N)])
